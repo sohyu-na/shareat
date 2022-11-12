850ff55f56fb8a6c21340fdb29f255a1415242a7
hyuni0316 <hyunini0408@gmail.com>
2022-11-12 21:10:46 +0900

    no message

--- app.py
+++ app.py
@@ -3,37 +3,27 @@ import sys
 app = Flask(__name__)
 
 # 메인홈
-
-
 @app.route("/")
 def main_home():
     return render_template("index.html")
 
 # 맛집 등록
-
-
-@app.route("/registration")
+@app.route("../templates/registerRestaurantInfo.html")
 def register_restaurant():
     return render_template("registerRestaurant.html")
 
 # 내가 찜한 밋집
-
-
 @app.route("/mylist")
 def register_restaurant():
     return render_template("myRestaurantList.html")
 
 # 로그인
-
-
-@app.route("/login")
+@app.route("../templates/login.html")
 def register_restaurant():
     return render_template("login.html")
 
 # 회원가입
-
-
-@app.route("/signup")
+@app.route("../templates/signup.html")
 def register_restaurant():
     return render_template("signup.html")
 
